0b95d55a961ef81441ddbd282b530279a1d1657f
Ayushi393 <as7384@srmist.edu.in>
2024-10-01 07:49:31 +0530

    weather app

---
+++ src/apiKeys.js
@@ -0,0 +1,7 @@
+// apiKeys.js
+const apiKeys = {
+  key: "c3d228c01db34dba05747bb7806794dc", // Use the key from the image
+  base: "https://api.openweathermap.org/data/3.0/",
+};
+
+export default apiKeys;
